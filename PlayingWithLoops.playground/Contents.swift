c26e60e36a39d452518ae766417cd0713369eebc
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-08-31 21:38:38 -0400

    little practice with loops

---
+++ PlayingWithLoops.playground/Contents.swift
@@ -0,0 +1,116 @@
+var i = 0
+
+while(true) {
+    i += 1
+    if (i >= 10) {
+        break
+    }
+}
+
+///
+
+var b = 70
+
+while (true) {
+    b +=  3
+    
+    if (b >= 1045) {
+        break
+    }
+}
+
+///
+
+var t = -45
+while (true) {
+    t += 2
+    if (t >= 0) {
+        break
+    }
+    
+}
+
+var v = 3
+
+while (true) {
+    
+    v *= 2
+    
+    if v >= 35 {
+        break
+    }
+}
+
+var m = 98908
+
+while(true) {
+    m /= 3
+    
+    if (m >= 7) {
+        break
+    }
+}
+
+
+i = 0
+
+while(i < 10) {
+    i += 1
+}
+
+//i = 0
+
+repeat {
+    i += 1
+} while(i < 10)
+
+
+
+for j in 0 ..< 20 {
+    j
+}
+
+let myArray = [1,5,10]
+
+for a in myArray {
+    a
+}
+
+
+for (var index = 1; index <= 15; index += 1) {
+    index
+}
+
+for index2 in 1...15 {
+    index2
+}
+
+for  var c in 0..<10 {
+    print(c)
+}
+
+var myNumbers: [Int] = [34, 23, 12, 114]
+for var p in myNumbers {
+    print(p)
+}
+
+var count1: Int = 0
+while count1 < 1001 {
+    print(count1)
+    count1 += 1
+}
+
+var count2: Int = 0
+repeat {
+    print("🍎")
+    count2 += 3
+}
+while count2 < 25
+
+
+var cookiesCount: Int = 789
+repeat {
+    print("You have got 🍪!" + " Enjoy Life! I wish you got more 🍪 though.")
+    cookiesCount -= 34
+}
+while cookiesCount > 0
